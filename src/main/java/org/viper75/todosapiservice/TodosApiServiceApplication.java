d0a95043657f0eca5e9707d5bd4b34e727e7d16c
viper75 <sean.huvaya03@yahoo.com>
2021-01-06 11:31:16 +0200

    Initial commit

---
+++ src/main/java/org/viper75/todosapiservice/TodosApiServiceApplication.java
@@ -0,0 +1,13 @@
+package org.viper75.todosapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodosApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/viper75/todosapiservice/TodosApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viper75.todosapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
